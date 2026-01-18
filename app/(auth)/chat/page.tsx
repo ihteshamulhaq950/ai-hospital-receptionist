893d19ffb8b494d7e29685ed0219ff76d4fdbba9
ihteshamulhaq950 <ihteshamhaq4@gmail.com>
2026-01-18 13:43:42 +0500

    Correct init api to create title

--- app/(auth)/chat/page.tsx
+++ app/(auth)/chat/page.tsx
@@ -41,7 +41,7 @@ export default function EmptyChatPage() {
       const res = await fetch("/api/chat/init", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        // body: JSON.stringify({ content }),
+        body: JSON.stringify({ title: content }),
       });
 
       const data = await res.json();
