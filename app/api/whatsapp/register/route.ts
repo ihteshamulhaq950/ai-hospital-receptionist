a4c68016e86419ca4669ffbf86bbe3b0bccdc655
ihteshamulhaq950 <ihteshamhaq4@gmail.com>
2026-02-15 20:34:55 +0500

    Edit whatsapp credentials

--- app/api/whatsapp/register/route.ts
+++ app/api/whatsapp/register/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
+  console.log("process.env.PHONE_NUMBER_ID:", process.env.WHATSAPP_PHONE_NUMBER_ID);
   try {
     const response = await fetch(
-      `https://graph.facebook.com/v24.0/${process.env.PHONE_NUMBER_ID}/register`,
+      `https://graph.facebook.com/v24.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/register`,
       {
         method: "POST",
         headers: {
@@ -17,6 +18,7 @@ export async function POST() {
       }
     );
 
+    console.log("WhatsApp registration response status:", response.status);
     const data = await response.json();
 
     return NextResponse.json(data);
